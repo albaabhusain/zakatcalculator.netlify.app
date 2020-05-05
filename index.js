763fc75cab3a0d9aaf276c7028a9f219452d8512
Albaab <husainalbaab2000@gmail.com>
2020-05-06 01:22:33 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,34 @@
+var gold=document.getElementById('gold');
+var silver=document.getElementById('silver');
+var property=document.getElementById('property');
+var savings=document.getElementById('saving');
+var result=document.getElementById('result');
+var button=document.getElementById('submit');
+var myform=document.getElementById('my-form');
+var reset=document.getElementById('reset');
+var out=document.getElementById('pararesult');
+
+
+
+
+ var totalzak=function(){
+
+    
+    const goldprice=4056*Number(gold.value)*0.025;
+    const silverprice=41*Number(silver.value)*0.025;
+    const propertyprice=Number(property.value)*0.025;
+    const savingsprice =Number(savings.value)*0.025;
+    const totalzak=goldprice+silverprice+propertyprice+savingsprice;
+
+  
+    result.value=totalzak;
+    out.innerText=totalzak;
+    myform.reset();
+
+ };
+ 
+ button.addEventListener('click',totalzak,false);
+//  button.addEventListener('click',()=>{
+//      myform.reset();
+//  },false);
+  
